2e9be0cb8376d80b999dbcd98291634fd8bee30c
slolo <mustaphason1991@outlook.fr>
2019-07-08 12:50:41 +0100

    modal modified , API added , message box added

--- main.js
+++ main.js
@@ -13,4 +13,31 @@ $( document ).ready(function() {
     })
 /*-------*/
 
+/*--- actual weight and motivatinal message ---*/
+$("#modal_form").submit(function(e){
+    e.preventDefault();
+    let actualWeight=$("#input-weight").val();   //had variable  radi na7tajoha 
+    let weightDate=$("#input-date").val(); //had variable  radi na7tajoha 
+    $(".span_value").text(actualWeight);
+    if (!actualWeight && !weightDate) return alert("one of the fields is empty");
+
+    /*-- motivational API --*/
+  
+  let http= new XMLHttpRequest(); 
+  http.onreadystatechange=function(){
+      if(http.readyState == 4 && http.status == 200){
+          let num=Math.floor(Math.random()*20);
+          $(".motivational_message").text(JSON.parse(http.response)[num].content);
+      }
+  }
+  http.open("GET","https://gist.githubusercontent.com/AbhishekChd/ab9949b618fcbf58ac84f9c8e88d6688/raw/90916f0d09a295eb5b2eea6c29a648e8c60b0e6f/5769a491e4b01190df7a9a70.json",true);
+  http.send();
 });
+})
+
+
+/*-- 9ra 3la ajax call  o chno howa json hna     https://www.w3schools.com/xml/ajax_intro.asp    ---*/ 
+/*-- had api rah radi nbadlaha 7it khditha rir man wa7ed app
+khasni ndir wa7da makhtasa rir b motivation -- */ 
+
+
